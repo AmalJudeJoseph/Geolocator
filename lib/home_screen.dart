fbb4bf5bc48145023067e7c459071f59a75e22c9
Amal Jude Joseph <amaljudejoseph@gmail.com>
2024-02-18 11:41:32 +0530

    Final Changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_tutorial/screens/current_location_screen.dart';
-import 'package:google_maps_flutter_tutorial/screens/nearby_places_screen.dart';
-import 'package:google_maps_flutter_tutorial/screens/search_places_screen.dart';
+
+
 import 'package:google_maps_flutter_tutorial/screens/simple_map_screen.dart';
 
-import 'screens/polyline_screen.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -37,27 +37,18 @@ class _HomeScreenState extends State<HomeScreen> {
               }));
             }, child: const Text("User current location")),
 
-            ElevatedButton(onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
-                return const SearchPlacesScreen();
-              }));
-            }, child: const Text("Search Places")),
+            FloatingActionButton(onPressed: (){},child:Icon(Icons.share),
+        ),
 
 
-            ElevatedButton(onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
-                return const NearByPlacesScreen();
-              }));
-            }, child: const Text("Near by Places")),
+            
 
 
-            ElevatedButton(onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
-                return const PolylineScreen();
-              }));
-            }, child: const Text("Polyline between 2 points"))
+            
           ],
         ),
+
+        
       ),
     );
   }
